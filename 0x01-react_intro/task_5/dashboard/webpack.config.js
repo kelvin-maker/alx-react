c9be570c9fb4be3fc4a1d5a12ba377c19e74a010
kelvin mwenda <62829439+kelvin-maker@users.noreply.github.com>
2023-02-02 11:57:16 +0300

    babel

---
+++ 0x01-react_intro/task_5/dashboard/webpack.config.js
@@ -0,0 +1,56 @@
+const path = require("path");
+const webpack = require("webpack");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCssAssetsWebpackPlugin = require("optimize-css-assets-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  devServer: {
+    hot: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader"
+        ]
+      },
+      {
+        test: /\.(png|jpeg|jpg|gif)$/,
+        use: {
+          loader: "file-loader",
+          options: {
+            outputPath: "assets"
+          }
+        }
+      }
+    ]
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    }),
+    new OptimizeCssAssetsWebpackPlugin({})
+  ]
+
+};
