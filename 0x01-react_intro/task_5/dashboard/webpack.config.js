6201bfa47d058926089b52cf92fd19c648412ea4
kelvin mwenda <62829439+kelvin-maker@users.noreply.github.com>
2023-02-02 11:52:16 +0300

    removed babel

--- 0x01-react_intro/task_5/dashboard/webpack.config.js
+++ 0x01-react_intro/task_5/dashboard/webpack.config.js
@@ -17,13 +17,6 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader"
-        }
-      },
-      {
         test: /\.css$/,
         use: [
           MiniCssExtractPlugin.loader,
